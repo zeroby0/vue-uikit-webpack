a195c757d637db6a61bd7dd686aa14dad4b01561
Aravind Reddy V <zerodividedby0@gmail.com>
2017-07-04 17:22:53 +0530

    First commit 🎉

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import UIkit from 'uikit'
+import Icons from 'uikit/dist/js/uikit-icons'
+UIkit.use(Icons)
+export default {
+  name: 'app'
+}
+</script>
+
+<style lang="less">
+@import './assets/theme/theme.less';
+</style>
---
+++ src/components/Hello.vue
@@ -0,0 +1,42 @@
+<template>
+<!-- Copied from here: https://getuikit.com/docs/dropdown#boundary -->
+
+<div class="boundary uk-panel uk-placeholder uk-width-2-3@s">
+
+    <button class="uk-button uk-button-default uk-float-right" type="button">Hover</button>
+    <div uk-dropdown="boundary: .boundary">
+        <ul class="uk-nav uk-dropdown-nav">
+            <li class="uk-active"><a href="#">Active</a></li>
+            <li><a href="#">Item</a></li>
+            <li class="uk-nav-header">Header</li>
+            <li><a href="#">Item</a></li>
+            <li><a href="#">Item</a></li>
+            <li class="uk-nav-divider"></li>
+            <li><a href="#">Item</a></li>
+        </ul>
+    </div>
+
+    <div class="uk-card uk-card-default uk-card-body uk-width-1-2@m">
+      <div class="uk-card-badge uk-label">Badge</div>
+      <h3 class="uk-card-title">Title</h3>
+      <p>Lorem ipsum color sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+    </div>
+
+</div>
+
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
